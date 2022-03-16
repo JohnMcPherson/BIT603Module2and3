2c8a4f6342a8303eb9b0f069a53534199fdab9be
JohnMcPherson <john.mcpherson@xtra.co.nz>
2022-03-16 13:05:10 +1300

    Change colour of buttons

--- app/src/main/java/academy/learnprogramming/bit603_exercises/MainActivity.java
+++ app/src/main/java/academy/learnprogramming/bit603_exercises/MainActivity.java
@@ -2,7 +2,10 @@ package academy.learnprogramming.bit603_exercises;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button redButton = findViewById(R.id.buttonRed);
+        final Button greenButton = findViewById(R.id.buttonGreen);
+
+        redButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redButton.setBackgroundColor(Color.RED);
+                greenButton.setBackgroundColor(Color.BLUE);
+            }
+        });
+
+        greenButton.setOnClickListener(v -> {
+            redButton.setBackgroundColor(Color.BLUE);
+            greenButton.setBackgroundColor(Color.GREEN);
+        });
     }
 }
